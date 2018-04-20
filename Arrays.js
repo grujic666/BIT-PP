fd86be1aafefd1f6e0252b52cd948c9d602da3ff
BIT student <classroom@bgit.rs>
2018-04-20 11:59:14 +0200

    Hello

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+var a = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(a[5], a[2], a[0]);
+
+
+var a = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+console.log(a[6]);
+
+var a = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log(a[1], a[4], a[5], a[6], a[8]);
+
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(a[3][2]);
+console.log(a[0][3])
