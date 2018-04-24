0f1a7116c12c32228b6133f0a2298d77c1a52003
BIT student <classroom@bgit.rs>
2018-04-24 18:03:06 +0200

    loops

---
+++ Loops/Loops.js
@@ -0,0 +1,27 @@
+
+
+
+for (var x = 0; x <= 15; x++) {
+    if (x % 2 === 0) {
+        console.log(x + " is even");
+    }
+    else {
+        console.log(x + " is odd");
+    }
+}
+var sum = 0
+for (var a = 0; a < 1000; a++) {
+    if (a % 3 === 0 || a % 5 === 0) {
+        sum += a;
+    }
+}
+console.log(sum);
+
+
+var b;
+var sum = 0
+var pro = 1
+var a = [4, 5, 2, 3];
+// sum: 14
+// pro: 120
+for (b = 0
---
+++ Loops/Loops2.js
@@ -0,0 +1,47 @@
+var a = [1, 2, 6, 5, 11];
+var i;
+var j;
+for (i = 0, j = a.length - 1; i < j; i++ , j--) {
+    var tmp;
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp;
+}
+console.log(a);
+
+var e = 3
+var a = [5, -4.2, 3, 7]
+var i;
+
+for (var i = 0; i < a.length; i++) {
+    if (a[i] == e) {
+        ind = true;
+    }
+}
+if (ind == false) {
+    console.log("No!");
+
+} else {
+    console.log("Yes!");
+}
+
+
+var a = [-3, 11, 5, 3.4, -8];
+var p;
+
+for (var p = 0; p < a.length; p++) {
+    if (a[p] > 0) {
+        a[p] = a[p] * 2
+    }
+}
+
+console.log(a);
+
+
+var a = [4, 2, 2, -1, 6];
+var i;
+var z = 0;
+
+for (var i = 0; i < a.length; i++) {
+    if (a[b] <)
+}
